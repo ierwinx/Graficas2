990b16aee143658a7b5b61bfc007619e0ec011dd
Eяw!и Gontier <ierwinx@me.com>
2022-10-07 13:04:09 -0500

    Initial Commit

---
+++ Graficas2/Graficas2App.swift
@@ -0,0 +1,17 @@
+//
+//  Graficas2App.swift
+//  Graficas2
+//
+//  Created by Erwin Luz León on 07/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Graficas2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
